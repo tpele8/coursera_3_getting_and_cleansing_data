d20293084cc27224149b2e864e3785993b2ec295
tpele8 <i64425@Tyler-Peterson.local>
2016-09-15 10:35:21 -0600

    Creating average dataset
    1. Removal of the measurement columns which were not associated with
    the mean and standard deviation
    
    2. Calculate the average value of each of the activity columns by
    activity and subject (this required removal of the activity description)
    
    3. Add the activity description back to the dataframe and create the
    final dataframe

--- course_project/script1.R
+++ course_project/script1.R
@@ -18,7 +18,6 @@
   colnames(yTest) <- c("activity_code")   
   yTestDF <- data.frame(activity_code = yTest$activity_code, activity = activityLabels[match(yTest$activity_code, activityLabels$activity_code), 2])
 
-  head(yTestDF)
   subjectTestDF <- read.table("/Users/i64425/Documents/Coursera/03 - Getting and Cleansing Data/Course Project/UCI HAR Dataset/test/subject_test.txt")
   colnames(subjectTestDF) <- c("subject_ID")
   
@@ -41,3 +40,39 @@
 
 ###### Combine test and train datasets ######
   fullDF <- rbind(fullTestDF, fullTrainDF)
+
+###### Remove measurement columns which are not associated with the mean and standard deviation ######
+  colsList <- grep("mean|std", value = TRUE, colnames(fullDF))
+  colsList2 <- c(colnames(fullDF)[1:3], colsList)
+  modDF <- fullDF[,colnames(fullDF)%in%colsList2]
+
+###### Calculate the average of each variable for each activity and each subject ######
+  ### Create an array of distinct subject_IDs ###
+  distinctSubject <- sort(unique(modDF$subject_ID))
+  
+  ### Create an array of distinct activities
+  distinctActivity <- sort(unique(modDF$activity_code))
+  
+  ### Create an array of measurement column names ###
+  dfColNames <- colnames(modDF)[4:length(colnames(modDF))]
+  
+  ### Calculate the average of the activity columns by subject_ID and activity code ###
+  avgValsDF <- data.frame()
+  for(i in 1:length(distinctSubject)){
+    tempDF1 <- subset(modDF, modDF$subject_ID == distinctSubject[i])
+    for(j in 1:length(distinctActivity)){
+      tempDF2 <- subset(tempDF1, tempDF1$activity_code == distinctActivity[j])
+      meanList1 <- list()
+      for(k in 1:length(dfColNames)){
+        meanColVal <- mean(tempDF2[[dfColNames[k]]])
+        meanList1 <- c(meanList1, meanColVal)
+      }
+      meanList2 <- c(distinctSubject[i], distinctActivity[j], meanList1)
+      avgValsDF <- rbind(avgValsDF, meanList2)
+    }
+  }
+  
+  ### Adding back the activity descriptions and creating the final dataframe ###
+  colnames(avgValsDF) <- c("subject_ID", "activity_code", dfColNames)
+  activityTempDF <- data.frame(subject_ID = avgValsDF$subject_ID, activity_code = avgValsDF$activity_code, activity = activityLabels[match(avgValsDF$activity_code, activityLabels$activity_code), 2])
+  finalDF <- cbind(activityTempDF, avgValsDF[,3:ncol(avgValsDF)])
